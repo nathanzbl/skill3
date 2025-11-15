123f07ee544b7017f5d74324110d1f7ead60a78f
nathanzbl <167259104+nathanzbl@users.noreply.github.com>
2025-11-15 14:11:30 -0700

    Add admin route with HTML response
    Added an admin route that displays an admin area page.

--- server.js
+++ server.js
@@ -12,6 +12,23 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/admin', (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <title>Admin Area</title>
+      </head>
+      <body>
+        <h1>Admin area</h1>
+        <p>If you can see this, your IP passed the Nginx allow list.</p>
+        <p>Domain: nathanblatter.is404.net</p>
+        <p>Path: /admin</p>
+      </body>
+    </html>
+  `);
+});
+
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Queue du Castor server is running on http://localhost:${PORT}`);
